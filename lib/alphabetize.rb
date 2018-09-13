afe028c959f9f5e295633293637b0bff71b2a698
Asaf Davidov <adavidov@conncoll.edu>
2018-09-13 20:46:35 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,7 @@
+require 'pry'
 def alphabetize(arr)
   # code here
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_alph_arr = esp_alph.chars
-  sorted_array = []
-  arr.collect do |phrase|
-    phrase
   end  
 end
